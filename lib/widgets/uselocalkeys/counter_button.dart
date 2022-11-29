64efb36b233ab7b879ac1c365b0ef9f5cb221f61
Miguel Ramos <return.infox@gmail.com>
2022-11-29 20:10:21 -0300

    CounterButton widget LocalKey

---
+++ lib/widgets/uselocalkeys/counter_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CounterButton extends StatefulWidget {
+  const CounterButton({super.key});
+
+  @override
+  State<CounterButton> createState() => _CounterButtonState();
+}
+
+class _CounterButtonState extends State<CounterButton> {
+  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        foregroundColor: Colors.white,
+        elevation: 3,
+        backgroundColor: Colors.indigoAccent
+        ),
+      onPressed: () {
+        setState(() {
+          _counter++;
+        });
+      },
+      child: Text(_counter.toString(), style: const TextStyle(fontSize: 35),),
+    );
+  }
+}
