70544bfa06b19b5387409f999e3c0021d75f75cd
Miguel Ramos <return.infox@gmail.com>
2022-11-29 19:25:09 -0300

    animatedtext terminado

--- lib/pages/localkeys/animated_text.dart
+++ lib/pages/localkeys/animated_text.dart
@@ -8,8 +8,42 @@ class AnimatedText extends StatefulWidget {
 }
 
 class _AnimatedTextState extends State<AnimatedText> {
+  int _counter = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    print('ejecutando build');
+    return Scaffold(
+      body: Center(
+        child: AnimatedSwitcher(
+          transitionBuilder: (child, animation) {
+            return ScaleTransition(
+              scale: animation,
+              child: child,
+            );
+          },
+          duration: const Duration(milliseconds: 200),
+          // child: _counter % 2 == 0 ?
+          //       const CircularProgressIndicator():
+          //       Text(_counter.toString(), style: const TextStyle(fontSize: 50))
+          child: Text(
+            // key: ValueKey<String>(_counter.toString()),
+            key: Key(_counter.toString()),
+            _counter.toString(), 
+            style: const TextStyle(fontSize: 50)
+            )
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _counter++;
+            //* recordemos que el setState vuelve a llamar al método build
+          });
+        },
+      ),
+    );
   }
 }
+
+//* AnimatedSwitcher anima un "nuevo widget"
+//* asegurarnos siempre que nuestros widgets secundarios tengan una clave(key)
