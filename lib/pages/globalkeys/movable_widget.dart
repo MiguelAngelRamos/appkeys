7d2760395d9b2360875c8d8ba7575e057f5fa3f2
Miguel Ramos <return.infox@gmail.com>
2022-12-01 20:02:54 -0300

    keys

--- lib/pages/globalkeys/movable_widget.dart
+++ lib/pages/globalkeys/movable_widget.dart
@@ -13,12 +13,19 @@ class _MovableWidgetState extends State<MovableWidget> {
 
   bool _inBody = true;
   final globalKeyCounter = GlobalKey<CounterButtonGlobalKeyState>();
-
+  final scaffoldkey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldkey,
       drawer: Drawer(),
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.widgets),
+          onPressed: () {
+            scaffoldkey.currentState?.openDrawer();
+          },
+          ),
         title: !_inBody ? CounterButtonGlobalKey(key: globalKeyCounter): null,
         backgroundColor: Colors.black87,
         actions: [
